adc4163e9291cb37f821f3b1101b78e42d7922db
evanmhenry <evanmhenry@gmail.com>
2024-05-16 10:25:22 -0400

    feat: setup shadcn

--- ReactExample.Web/src/App.tsx
+++ ReactExample.Web/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div className=" bg-blue-700 text-red-600">
       Hello
+      <Button>Test</Button>
     </div>
   );
 };
