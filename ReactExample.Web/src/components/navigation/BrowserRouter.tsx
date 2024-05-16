56064b6d992b9cb6e052e3fc7e0599d4035d728a
evanmhenry <evanmhenry@gmail.com>
2024-05-16 13:07:39 -0400

    feat: adds new routes

--- ReactExample.Web/src/components/navigation/BrowserRouter.tsx
+++ ReactExample.Web/src/components/navigation/BrowserRouter.tsx
@@ -3,6 +3,9 @@ import MainLayout from "./MainLayout";
 import DashboardPage from "@/pages/DashboardPage";
 import SettingsPage from "@/pages/SettingsPage";
 import ErrorPage from "@/pages/ErrorPage";
+import ChartsPage from "@/pages/ChartsPage";
+import CardsPage from "@/pages/CardsPage";
+import DataTablePage from "@/pages/DataTablePage";
 
 const BrowserRouter = createBrowserRouter([
   {
@@ -12,6 +15,18 @@ const BrowserRouter = createBrowserRouter([
       {
         path: "/dashboard",
         element: <DashboardPage />,
+      },
+      {
+        path: "/charts",
+        element: <ChartsPage />,
+      },
+      {
+        path: "/data-table",
+        element: <DataTablePage />,
+      },
+      {
+        path: "/cards",
+        element: <CardsPage />,
       },
       {
         path: "settings",
