eb7e7d71cda933a39d9a0aada354bdfce6f9d844
Evan Henry <evan@spadehq.com>
2024-05-18 07:38:37 -0400

    fixing main file naming issue

--- ReactExample.Web/src/Main1.tsx
+++ ReactExample.Web/src/Main1.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "./components/theming/ThemeProvider.tsx";
 import { RouterProvider } from "react-router-dom";
 import BrowserRouter from "./components/navigation/BrowserRouter.tsx";
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <RouterProvider router={BrowserRouter} />
