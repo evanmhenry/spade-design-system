af391cd3ef75baf7bac2a9ac9ea5664f2cb822c8
evanmhenry <evanmhenry@gmail.com>
2024-05-29 15:54:43 -0400

    feat: adds support for dark mode, makes header fixed and adds a footer

--- ReactExample.Web/src/pages/LandingPage.tsx
+++ ReactExample.Web/src/pages/LandingPage.tsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 
 const MenuButton = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => (
 	<button onClick={() => setIsOpen(!isOpen)} className='flex flex-col w-6 h-6 justify-center items-center gap-2'>
-		<div className={`w-5 h-[2px] rounded bg-gray-800 transition-transform duration-200 ${isOpen ? 'transform rotate-45 translate-y-1.5' : ''}`}></div>
-		<div className={`w-5 h-[2px] rounded bg-gray-800 transition-transform duration-200 ${isOpen ? 'transform -rotate-45 -translate-y-1' : ''}`}></div>
+		<div className={`w-5 h-[2px] rounded bg-primary transition-transform duration-200 ${isOpen ? 'transform rotate-45 translate-y-1.5' : ''}`}></div>
+		<div className={`w-5 h-[2px] rounded bg-primary transition-transform duration-200 ${isOpen ? 'transform -rotate-45 -translate-y-1' : ''}`}></div>
 	</button>
 )
 
@@ -31,12 +31,12 @@ const LandingPage = () => {
 
 	return (
 		<div className='h-dvh'>
-			<header className={`relative flex flex-col container border-b items-center transition-all py-4 duration-200 ${isOpen ? 'h-dvh' : 'h-16'}`}>
+			<header className={`fixed top-0 w-full container border-b transition-all py-4 duration-300 backdrop-blur-xl ${isOpen ? 'h-screen' : 'h-16'}`}>
 				<div id='menu' className='flex justify-between w-full items-center'>
-					<h1 className='text-2xl font-bold'>React Pattern</h1>
+					<h1 className='text-2xl font-bold text-primary'>React Pattern</h1>
 					<MenuButton isOpen={isOpen} setIsOpen={setIsOpen} />
 				</div>
-				<div className={`overflow-hidden transition-all duration-200 ${isOpen ? 'h-full' : 'max-h-0'} w-full gap-4 pt-6`}>
+				<div className={`overflow-hidden transition-all duration-300 ${isOpen ? 'h-full' : 'max-h-0'} w-full gap-4 pt-6`}>
 					<div className='flex flex-col gap-8'>
 						<div className='flex flex-col gap-4'>
 							<h3 className='text-sm text-muted-foreground'>Docs</h3>
@@ -59,11 +59,11 @@ const LandingPage = () => {
 					</div>
 				</div>
 			</header>
-			<div className='bg-white h-dvh'>
+			<div className='pt-16 bg-background h-dvh'>
 				<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
 					<div className='text-center'>
-						<h2 className='text-3xl font-extrabold text-gray-900 sm:text-4xl'>Welcome to React Pattern</h2>
-						<p className='mt-4 text-lg leading-6 text-gray-500'>This is an example project to demonstrate react conventions.</p>
+						<h2 className='text-3xl font-extrabold text-primary sm:text-4xl'>Welcome to React Pattern</h2>
+						<p className='mt-4 text-lg leading-6 text-muted-foreground'>This is an example project to demonstrate react conventions.</p>
 						<div className='mt-6'>
 							<Link to='/buttons'>
 								<Button>Go to dashboard</Button>
@@ -72,6 +72,13 @@ const LandingPage = () => {
 					</div>
 				</div>
 			</div>
+			<footer className='relative border-t z-10'>
+				<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 backdrop-blur-lg'>
+					<div className='text-center'>
+						<p className='text-sm text-muted-foreground'>React Pattern</p>
+					</div>
+				</div>
+			</footer>
 		</div>
 	)
 }
