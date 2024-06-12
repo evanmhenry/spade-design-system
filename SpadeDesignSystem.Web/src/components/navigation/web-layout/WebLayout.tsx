4ddba8871bfa0866a73b3cd931c8dfed1ff848c4
evanmhenry <evanmhenry@gmail.com>
2024-06-12 12:53:35 -0400

    refactor: moves web layout components into a folder

--- SpadeDesignSystem.Web/src/components/navigation/web-layout/WebLayout.tsx
+++ SpadeDesignSystem.Web/src/components/navigation/web-layout/WebLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const WebLayout = () => {
 	const [isOpen, setIsOpen] = useState(false)
