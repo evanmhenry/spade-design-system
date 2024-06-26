9500626540572ce0bb5d6a975163fdcbbb037429
Evan Henry <evan@spadehq.com>
2024-06-26 10:34:57 -0400

    adds brand variables to index.css

--- SpadeDesignSystem.Web/src/components/ui/Button.tsx
+++ SpadeDesignSystem.Web/src/components/ui/Button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
 		variants: {
 			variant: {
 				default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+				brand: 'bg-brand text-brand-foreground hover:bg-brand/90',
 				success: 'bg-success text-success-foreground hover:bg-success/90',
 				warning: 'bg-warning text-warning-foreground hover:bg-warning/90',
 				destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
@@ -23,7 +24,7 @@ const buttonVariants = cva(
 			size: {
 				default: 'h-10 px-4 py-2',
 				sm: 'h-9 rounded-md px-3',
-				lg: 'h-11 rounded-md px-8',
+				lg: 'h-11 rounded-md px-8 text-md',
 				icon: 'h-10 w-10',
 			},
 		},
