d70903a95c11606fea888182f7fbc26c4710518f
Evan Henry <evan@spadehq.com>
2024-06-26 10:37:39 -0400

    feat: react table add

--- SpadeDesignSystem.Web/tailwind.config.js
+++ SpadeDesignSystem.Web/tailwind.config.js
@@ -21,6 +21,10 @@ export const theme = {
       ring: "hsl(var(--ring))",
       background: "hsl(var(--background))",
       foreground: "hsl(var(--foreground))",
+      brand: {
+        DEFAULT: "hsl(var(--brand))",
+        foreground: "hsl(var(--brand-foreground))",
+      },
       primary: {
         DEFAULT: "hsl(var(--primary))",
         foreground: "hsl(var(--primary-foreground))",
