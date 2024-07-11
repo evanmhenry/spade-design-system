f2d5c3b36d9b6478439736eb0f293fd74f19e0a0
Evan Henry <evan@spadehq.com>
2024-07-11 11:53:49 -0400

    feat: adds search button and layout to header component

--- SpadeDesignSystem.Web/src/components/navigation/web-layout/components/HeaderV2.tsx
+++ SpadeDesignSystem.Web/src/components/navigation/web-layout/components/HeaderV2.tsx
@@ -3,50 +3,94 @@ import Logo from '@/assets/spade-design-system-box.svg'
 import { Button } from '@/components/ui/Button'
 import { HashLink } from 'react-router-hash-link'
 import { NavMenuOptions } from '@/pages/landingPage/Content'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import ThemeToggleV2 from '@/components/theming/ThemeToggleV2'
 import { GitHubIcon } from '@/components/icons/SocialIcon'
 import { Separator } from '@/components/ui/Separator'
 import ThemeToggleV3 from '@/components/theming/ThemeToggleV3'
+import { Search } from 'lucide-react'
+import SearchDialog from './SearchDialog'
 
 const HeaderV2 = () => {
-	const [isOpen, setIsOpen] = useState(false)
 	const navigate = useNavigate()
+	const location = useLocation()
+	const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
+	const [searchOpen, setSearchOpen] = useState(false)
+
+	const handleSearch = () => {
+		setSearchOpen((searchOpen) => !searchOpen)
+	}
+
+	useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault()
+				setSearchOpen((searchOpen) => !searchOpen)
+			}
+		}
+
+		document.addEventListener('keydown', down)
+		return () => document.removeEventListener('keydown', down)
+	}, [])
+
+	const isDocsLayout = () => {
+		return location.pathname.startsWith('/docs')
+	}
 
 	return (
-		<header className={`z-20 sticky top-0 w-full border-b transition-all duration-300 bg-background ${isOpen ? 'h-screen' : 'h-16'}`}>
-			<div className='container max-w-screen-2xl'>
-				<div className='flex justify-between w-full items-center h-16'>
-					<a href='/'>
-						<div className='flex items-center gap-2 text-2xl font-semibold'>
-							<img alt='Logo' className='h-8 flex-none hover:cursor-pointer' src={Logo} onClick={() => navigate('/')} /> Spade UI
-						</div>
-					</a>
-					<HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} className='md:hidden' />
-					<div className='hidden md:flex gap-2 md:items-center'>
-						{NavMenuOptions.map((option) => (
-							<Button variant='brandGhost' size='sm' key={option.name}>
-								<option.type to={option.path}>{option.name}</option.type>
-							</Button>
-						))}
-						<Separator orientation='vertical' className='h-6 ml-2' />
-						<div className='flex gap-2 pl-2 items-center text-muted-foreground'>
-							<ThemeToggleV2 />
-							<GitHubIcon />
+		<>
+			<header className={`z-20 sticky top-0 w-full border-b transition-all duration-300 bg-background ${hamburgerMenuOpen ? 'h-screen' : 'h-16'}`}>
+				<div className='container max-w-screen-2xl'>
+					<div className='flex justify-between w-full items-center h-16'>
+						<div className='flex items-center justify-between gap-1 w-full '>
+							<div className='flex items-center gap-4 w-full justify-between'>
+								<a href='/'>
+									<div className='flex items-center gap-2 text-2xl font-semibold'>
+										<img alt='Logo' className='h-8 flex-none hover:cursor-pointer' src={Logo} onClick={() => navigate('/')} />{' '}
+										<span className={`${isDocsLayout() ? 'hidden' : ''} md:flex`}>Spade UI</span>
+									</div>
+								</a>
+								<Button
+									variant='outline'
+									size='sm'
+									className={`${isDocsLayout() ? '' : 'hidden'} md:flex text-muted-foreground gap-2 text-left w-56 justify-between`}
+									onClick={handleSearch}
+								>
+									<div className='flex items-center gap-2'>
+										<Search size={16} strokeWidth={2} />
+										Search...
+									</div>
+									<kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+										<span className='text-xs'>⌘</span>K
+									</kbd>
+								</Button>
+								<HamburgerMenu isOpen={hamburgerMenuOpen} setIsOpen={setHamburgerMenuOpen} className='md:hidden' />
+							</div>
+							<div className='hidden md:flex gap-1 md:items-center'>
+								{NavMenuOptions.map((option) => (
+									<Button variant='ghost' key={option.name}>
+										<option.type to={option.path}>{option.name}</option.type>
+									</Button>
+								))}
+								<Separator orientation='vertical' className='h-6 ml-2' />
+								<div className='flex gap-1 pl-2 items-center text-muted-foreground'>
+									<ThemeToggleV2 />
+									<GitHubIcon />
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{/* DROP DOWN MENU */}
-				<div
-					className={`overflow-hidden transition-all duration-500 ease-in-out transform ${
-						isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
-					} w-full gap-4 pt-6 bg-background`}
-				>
-					<div className='flex flex-col gap-8'>
-						<div className='flex flex-col gap-3'>
-							{/* <Button type='button' variant='brandGradient' size='lg' onClick={handlePrimaryCtaClick}>
+					{/* DROP DOWN MENU */}
+					<div
+						className={`overflow-hidden transition-all duration-500 ease-in-out transform ${
+							hamburgerMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
+						} w-full gap-4 pt-6 bg-background`}
+					>
+						<div className='flex flex-col gap-8'>
+							<div className='flex flex-col gap-3'>
+								{/* <Button type='button' variant='brandGradient' size='lg' onClick={handlePrimaryCtaClick}>
 								Contact Sales
 							</Button>
 							<Button
@@ -57,19 +101,21 @@ const HeaderV2 = () => {
 							>
 								Sign in
 							</Button> */}
-							<ThemeToggleV3 />
-						</div>
-						<div className='flex flex-col gap-6'>
-							{NavMenuOptions.map((option) => (
-								<div key={option.name} className='text-3xl font-medium'>
-									<HashLink to={option.path}>{option.name}</HashLink>
-								</div>
-							))}
+								<ThemeToggleV3 />
+							</div>
+							<div className='flex flex-col gap-6'>
+								{NavMenuOptions.map((option) => (
+									<div key={option.name} className='text-3xl font-medium'>
+										<HashLink to={option.path}>{option.name}</HashLink>
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</header>
+			</header>
+			<SearchDialog open={searchOpen} setOpen={setSearchOpen} />
+		</>
 	)
 }
 
