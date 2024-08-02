0a386d3bc41774c6cfb160d0d673af09e5d1cd49
Evan Henry <evan@spadehq.com>
2024-08-01 21:58:11 -0400

    updates date picker and popover

--- SpadeDesignSystem.Web/src/components/ui/DatePicker.tsx
+++ SpadeDesignSystem.Web/src/components/ui/DatePicker.tsx
@@ -1,26 +1,34 @@
-import * as React from 'react'
 import { format } from 'date-fns'
 import { Calendar as CalendarIcon } from 'lucide-react'
 import { cn } from '@/lib/Utils'
 import { Button } from '@/components/ui/Button'
+import { Calendar } from '@/components/ui/Calendar'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/Popover'
-import { Calendar } from './Calendar'
+import { useState } from 'react'
 
-export function DatePickerDemo() {
-	const [date, setDate] = React.useState<Date>()
+type DatePickerProps = {
+	label?: string
+}
+
+const DatePicker = ({ label }: DatePickerProps) => {
+	const [date, setDate] = useState<Date>()
 
 	return (
-		<Popover>
-			<PopoverTrigger asChild>
-				<Button variant={'outline'} className={cn('w-[280px] justify-start text-left font-normal', !date && 'text-muted-foreground')}>
-					<CalendarIcon className='mr-2 h-4 w-4' />
-					{date ? format(date, 'PPP') : <span>Pick a date</span>}
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className='w-auto p-0'>
-				<Calendar mode='single' selected={date} onSelect={setDate} initialFocus />
-			</PopoverContent>
-		</Popover>
+		<div>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button variant={'outline'} className={cn('w-[280px] justify-start text-left font-normal', !date && 'text-muted-foreground')}>
+						<CalendarIcon className='mr-2 h-4 w-4' />
+						{date ? format(date, 'PPP') : <span>{label || 'Select a date'}</span>}
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className='w-auto p-0'>
+					<Calendar mode='single' selected={date} onSelect={setDate} initialFocus />
+				</PopoverContent>
+			</Popover>
+		</div>
 	)
 }
+
+export default DatePicker
 
