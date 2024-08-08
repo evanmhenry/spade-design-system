0b6c695995e09ec6498f4811ceff3709a3724127
Evan Henry <evan@spadehq.com>
2024-08-08 12:44:09 -0400

    style: updates data table

--- SpadeDesignSystem.Web/src/components/ui/DataTable/DataTable.tsx
+++ SpadeDesignSystem.Web/src/components/ui/DataTable/DataTable.tsx
@@ -77,10 +77,13 @@ const DataTable = ({ title, data, columns, searchFilter = [], addLabel, onAdd, o
 		}
 	}
 
+	const isOnePage = table.getPageCount() <= 1
+	const NUM_DISPLAYING_CURRENTLY = table.getRowModel().rows.length
+	const TOTAL_ENTRIES = data?.length || 0
+
 	return (
 		<div className='w-full'>
 			<div className='flex items-center pb-4 justify-between'>
-				{/* TODO: add global search / search multiple columns */}
 				<h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>{title}</h3>
 				<div className='flex items-center space-x-2'>
 					{searchFilter.length > 0 && <Input placeholder='Search...' value={searchValue} onChange={(event) => searchTable(event.target.value)} className='max-w-sm' />}
@@ -104,55 +107,55 @@ const DataTable = ({ title, data, columns, searchFilter = [], addLabel, onAdd, o
 						</DropdownMenu>
 					)}
 					{onAdd && (
-						<Button variant='brand' onClick={onAdd} disabled={addDisabled}>
+						<Button onClick={onAdd} variant='brand' disabled={addDisabled}>
 							<Plus className='mr-2 h-5 w-5' />
 							{addLabel}
 						</Button>
 					)}
 				</div>
 			</div>
-			<div className='rounded-md border'>
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => (
-									<TableHead key={header.id}>{header.isPlaceholder ? undefined : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<TableHead key={header.id}>{header.isPlaceholder ? undefined : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+							))}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{NUM_DISPLAYING_CURRENTLY ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'} onClick={() => handleRowClick(row.original)}>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
 								))}
 							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'} onClick={() => handleRowClick(row.original)}>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-									))}
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell colSpan={columns.length} className='h-24 text-center'>
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className='h-24 text-center'>
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 			<div className='flex items-center justify-end space-x-2 py-4'>
 				<div className='flex-1 text-sm text-muted-foreground'>
-					{/* {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected. */}
+					Showing: <span className='font-semibold'>{NUM_DISPLAYING_CURRENTLY}</span> of {TOTAL_ENTRIES}
 				</div>
-				<div className='space-x-2'>
-					<Button variant='outline' size='sm' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-						Previous
-					</Button>
-					<Button variant='outline' size='sm' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-						Next
-					</Button>
-				</div>
+				{!isOnePage && (
+					<div className='space-x-2'>
+						<Button variant='outline' size='sm' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+							Previous
+						</Button>
+						<Button variant='outline' size='sm' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+							Next
+						</Button>
+					</div>
+				)}
 			</div>
 		</div>
 	)
